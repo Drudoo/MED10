83e6142b6f59d2552e98375ca70ee3ed60868c7a
Frederik Vanggaard <drudoo@drudoo.com>
2016-04-05 11:33:20 +0200

    Added images to buttons and deleted old images.

---
+++ MED10/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+
+	public Image myButtonImage; // Assign /Initilize from Editor or code
+	Button myButton;
+	void Awake() {
+		myButton = GetComponent<Button>();
+		myButton.image = myButtonImage;
+	}
+}
