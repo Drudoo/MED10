ad4d68e0915c1516ed5c7c1de6979747254ea096
Frederik Vanggaard <drudoo@drudoo.com>
2016-04-05 11:22:05 +0200

    Added change scene script and scenes for Educator and Student

--- MED10/Assets/Scripts/ChangeScene.cs
+++ MED10/Assets/Scripts/ChangeScene.cs
@@ -1,15 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
-	void Update () {
-	
+	public void ChangeToScene (string sceneToChangeTo) {
+		SceneManager.LoadScene(sceneToChangeTo);
 	}
 }
