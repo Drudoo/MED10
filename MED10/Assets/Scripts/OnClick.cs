b14bd1064c323f9bd8855a21837ea7cd7c5a8a0b
Frederik Vanggaard <drudoo@drudoo.com>
2016-04-06 12:56:53 +0200

    Buttons cant be pressed when popup is visible

--- MED10/Assets/Scripts/OnClick.cs
+++ MED10/Assets/Scripts/OnClick.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class OnClick : MonoBehaviour {
 
@@ -11,11 +12,17 @@ public class OnClick : MonoBehaviour {
 		private string username;
 		private string password;
 
+		private Button _eduB;
+		private Button _stuB;
+
 		public void showWindow(string newInfo) {
 			Info = newInfo;
 			username="Enter Username";
 			password="";
-
+			GameObject eduB = GameObject.Find("Educator");
+			_eduB = eduB.GetComponent<Button>();
+			GameObject stuB = GameObject.Find("Student");
+			_stuB = stuB.GetComponent<Button>();
 			PopUp = true;
 		}
 
@@ -27,11 +34,14 @@ public class OnClick : MonoBehaviour {
 
 			Rect save = new Rect(1084/2-300/2+100, 360, 100, 20);
 
-			if (PopUp)
-			{
+			if (PopUp) {
+					_eduB.interactable = false;
+					_stuB.interactable = false;
 					GUI.Box(rect, Info);
-					if (GUI.Button(close,"X"))
-					{
+					if (GUI.Button(close,"X")) {
+							_eduB.interactable = true;
+							_stuB.interactable = true;
+
 							PopUp = false;
 					}
 					string _username = GUI.TextField(usr, username, 25);
@@ -42,6 +52,8 @@ public class OnClick : MonoBehaviour {
 					if(GUI.Button(save,"Save")) {
 						Debug.Log("Username: " + username);
 						Debug.Log("Password: " + password);
+						_eduB.interactable = true;
+						_stuB.interactable = true;
 						PopUp = false;
 					}
 
