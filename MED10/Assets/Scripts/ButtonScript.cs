fe2be5c2a3eec7267709949283fd5e1c57436f4a
Frederik Vanggaard <drudoo@drudoo.com>
2016-04-11 16:20:54 +0200

    Updated with various scrips.

--- MED10/Assets/Scripts/ButtonScript.cs
+++ MED10/Assets/Scripts/ButtonScript.cs
@@ -4,10 +4,25 @@ using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour {
 
-	public Image myButtonImage; // Assign /Initilize from Editor or code
-	Button myButton;
-	void Awake() {
-		myButton = GetComponent<Button>();
-		myButton.image = myButtonImage;
+	// Assign /Initilize from Editor or code
+	public Button _button;
+	public Texture _image;
+	public string _text;
+	private float x,y,width,height;
+
+	void Start() {
+		RectTransform rt = (RectTransform)_button.transform;
+		width = rt.rect.width;
+		height = rt.rect.height;
+		x = _button.transform.position.x;
+		y = _button.transform.position.y;
+		Debug.Log("x: " + x + " y: " + y + " w: " + width + " h: " + height);
+	}
+
+	void OnGUI() {
+		GUI.BeginGroup (new Rect (x, y, 100, 100));
+		GUI.Box (new Rect (0,0,100,100), "Group is here");
+    	GUI.Button (new Rect (10,40,80,30), _image);
+		GUI.EndGroup();
 	}
 }
---
+++ MED10/Assets/Scripts/aspectRatio.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class aspectRatio : MonoBehaviour {
+
+	public float aspectX = 4f;
+	public float aspectY = 3f;
+
+	// Use this for initialization
+	void Start () {
+		// set the desired aspect ratio
+		float targetaspect = aspectX / aspectY;
+		// determine the game window's current aspect ratio
+		float windowaspect = (float)Screen.width / (float)Screen.height;
+		// current viewport height should be scaled by this amount
+		float scaleheight = windowaspect / targetaspect;
+		// obtain camera component so we can modify its viewport
+		Camera camera = GetComponent<Camera>();
+		if (scaleheight < 1.0f)
+	    {
+	        Rect rect = camera.rect;
+
+	        rect.width = 1.0f;
+	        rect.height = scaleheight;
+	        rect.x = 0;
+	        rect.y = (1.0f - scaleheight) / 2.0f;
+
+	        camera.rect = rect;
+	    }
+	    else // add pillarbox
+	    {
+	        float scalewidth = 1.0f / scaleheight;
+
+	        Rect rect = camera.rect;
+
+	        rect.width = scalewidth;
+	        rect.height = 1.0f;
+	        rect.x = (1.0f - scalewidth) / 2.0f;
+	        rect.y = 0;
+
+	        camera.rect = rect;
+	    }
+	}
+
+}
