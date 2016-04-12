0c4159e9b158431accef8f72e4ee0e607234fde5
Frederik Vanggaard <drudoo@drudoo.com>
2016-04-12 13:39:17 +0200

    Updated

---
+++ MED10/Assets/Scripts/AndroidDialog.cs
@@ -0,0 +1,117 @@
+using UnityEngine;
+using System.Collections;
+
+public class AndroidDialog : MonoBehaviour {
+
+ const int ButtonWidth = 256;
+ const int ButtonHeight = 64;
+
+ private string mMessage = "See My Android Dialog";
+
+ private bool mYesPressed = false;
+ private bool mNoPressed = false;
+
+ // Use this for initialization
+ void Start () {
+ }
+
+ // Update is called once per frame
+ void Update () {
+ }
+
+ void OnGUI() {
+
+  if (GUI.Button(new Rect((Screen.width-ButtonWidth)/2, (Screen.height-ButtonHeight)/2,
+                           ButtonWidth, ButtonHeight),
+                 "ShowDialog")) {
+   showDialog();
+  }
+
+  if (mYesPressed) {
+   GUI.Label(new Rect((Screen.width-ButtonWidth)/2, (Screen.height-ButtonHeight)/2 + 2 * ButtonHeight,
+                      ButtonWidth, ButtonHeight), "You Pressed Yes");
+  }
+
+  if (mNoPressed) {
+   GUI.Label(new Rect((Screen.width-ButtonWidth)/2, (Screen.height-ButtonHeight)/2 + 2 * ButtonHeight,
+                      ButtonWidth, ButtonHeight), "You Pressed No");
+  }
+ }
+
+ // Lets put our android specific code under the macro UNITY_ANDROID
+#if UNITY_ANDROID
+ // Lets create some listners.
+ // These listners will be passed to android
+
+ // Create the postive action listner class
+ // It has to be derived from the AndroidJavaProxy class
+ // Make the methods as same as that of DialogInterface.OnClickListener
+ private class PositiveButtonListner : AndroidJavaProxy {
+  private AndroidDialog mDialog;
+
+  public PositiveButtonListner(AndroidDialog d)
+   : base("android.content.DialogInterface$OnClickListener") {
+   mDialog = d;
+  }
+
+  public void onClick(AndroidJavaObject obj, int value ) {
+   mDialog.mYesPressed = true;
+   mDialog.mNoPressed = false;
+  }
+ }
+
+ // Create the postive action listner class
+ // It has to be derived from the AndroidJavaProxy class
+ // Make the methods as same as that of DialogInterface.OnClickListener
+ private class NegativeButtonListner : AndroidJavaProxy {
+  private AndroidDialog mDialog;
+
+  public NegativeButtonListner(AndroidDialog d)
+  : base("android.content.DialogInterface$OnClickListener") {
+   mDialog = d;
+  }
+
+  public void onClick(AndroidJavaObject obj, int value ) {
+   mDialog.mYesPressed = false;
+   mDialog.mNoPressed = true;
+  }
+ }
+
+
+#endif
+
+ private void showDialog() {
+
+#if UNITY_ANDROID
+  // Obtain activity
+  AndroidJavaClass unityPlayer = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+  AndroidJavaObject activity = unityPlayer.GetStatic< AndroidJavaObject>  ("currentActivity");
+
+  // Lets execute the code in the UI thread
+  activity.Call("runOnUiThread", new AndroidJavaRunnable(() =>  {
+
+   // Create an AlertDialog.Builder object
+   AndroidJavaObject alertDialogBuilder = new AndroidJavaObject("android/app/AlertDialog$Builder", activity);
+
+   // Call setMessage on the builder
+   alertDialogBuilder.Call< AndroidJavaObject> ("setMessage", mMessage);
+
+   // Call setCancelable on the builder
+   alertDialogBuilder.Call< AndroidJavaObject> ("setCancelable", true);
+
+   // Call setPositiveButton and set the message along with the listner
+   // Listner is a proxy class
+   alertDialogBuilder.Call< AndroidJavaObject> ("setPositiveButton", "Yes", new PositiveButtonListner(this));
+
+   // Call setPositiveButton and set the message along with the listner
+   // Listner is a proxy class
+   alertDialogBuilder.Call< AndroidJavaObject> ("setNegativeButton", "No", new NegativeButtonListner(this));
+
+   // Finally get the dialog instance and show it
+   AndroidJavaObject dialog = alertDialogBuilder.Call< AndroidJavaObject> ("create");
+   dialog.Call("show");
+  }));
+#endif
+
+ }
+ }
--- MED10/Assets/Scripts/QuizGame.cs
+++ MED10/Assets/Scripts/QuizGame.cs
@@ -1,11 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.IO;
 
 public class QuizGame : MonoBehaviour {
 
 	private InputField _questions;
 	private InputField _answers;
+	string[] objects = {"Question", "Save", "OptionA", "OptionB", "OptionC", "OptionD", "OptionE", "OptionF", "OptionG"};
+
+	void Start() {
+		for (int i = 0; i < 9; i++) {
+			GameObject _object = GameObject.Find(objects[i]);
+			_object.transform.localScale = new Vector3(0, 0, 0);
+		}
+	}
 
 	public void Generate() {
 		GameObject questions = GameObject.Find("NumberOfQuestions");
@@ -25,9 +34,18 @@ public class QuizGame : MonoBehaviour {
 
 		Debug.Log("Q: " + _valueQ + " A: " + _valueA);
 
-		for (int i = 0; i < _valueQ; i++) {
-
+		for (int i = 0; i < _valueA+2; i++) {
+			GameObject _object = GameObject.Find(objects[i]);
+			_object.transform.localScale = new Vector3(1, 1, 1);
 		}
-}
+
+		for (int i = 0; i < _valueQ; i++) {
+			inputQuestion();
+		}
+	}
+
+	private void inputQuestion() {
+
+	}
 
 }
